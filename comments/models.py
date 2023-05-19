f3413c8c467ba7ca32c3d081c7019ad23e4f586f
Lee Hyeonsik <hyeon5819@gmail.com>
2023-05-19 17:16:10 +0900

    model 작성

--- comments/models.py
+++ comments/models.py
@@ -1,7 +1,8 @@
 from django.db import models
+from users.models import CommonModel
 
 # Create your models here.
-class Comment(models.Model):
+class Comment(CommonModel):
     # writer = models.ForeignKey(User, on_delete=models.CASCADE)
     # music = models.ForeignKey(Article, on_delete=models.CASCADE, related_name="comments")
     comment = models.TextField("댓글")
