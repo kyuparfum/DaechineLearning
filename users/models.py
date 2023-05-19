ab3f48b451b0b7b827ec53a5c3804f2e866bb1be
HYnllvo <gpalsu0812@naver.com>
2023-05-19 19:58:39 +0900

    users-CommonModel db_status 추가

--- users/models.py
+++ users/models.py
@@ -2,9 +2,13 @@ from django.db import models
 
 # Create your models here.
 class CommonModel(models.Model):
+    db_status_choice = [
+        (1, 'active'), 
+        (2, 'delete'),
+    ]
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
-    # db_status??
+    db_status = models.PositiveIntegerField(max_length=1, choices=db_status_choice, default=1)
     
     class Meta:
         abstract = True
