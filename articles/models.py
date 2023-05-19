d403df53246b0f993f2b21557ddf965c330e9fc8
HYnllvo <gpalsu0812@naver.com>
2023-05-19 20:00:29 +0900

    게시물모델명 수정/Articles->Article

--- articles/models.py
+++ articles/models.py
@@ -3,7 +3,7 @@ from users.models import CommonModel
 # Create your models here.
 
 
-class Articles(CommonModel):
+class Article(CommonModel):
     # 작성자-User모델
     # writer = models.ForeignKey(User, on_delete=models.CASCADE)  #related_name 필요할까요?
     # 음악-Music모델
