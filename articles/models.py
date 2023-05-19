d5e3510b8f790c8f62e35f10bef3acb0181911e1
HYnllvo <gpalsu0812@naver.com>
2023-05-19 17:09:44 +0900

    model 작성

--- articles/models.py
+++ articles/models.py
@@ -1,16 +1,12 @@
 from django.db import models
-
+from users.models import CommonModel
 # Create your models here.
-class CommonModel(models.Model):
-    created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=True)
-    
-    class Meta:
-        abstract = True
-        # class Meta 를 선언함으로써, 다른 모델들이 상속 받을수 있는 모델이 됨
+
 
 class Articles(CommonModel):
-    # user = models.ForeignKey(User, on_delete=models.CASCADE)  #related_name 작성할까요말까요
+    # 작성자-User모델
+    # writer = models.ForeignKey(User, on_delete=models.CASCADE)  #related_name 필요할까요?
+    # 음악-Music모델
     # music = models.ForeignKey(Music, on_delete=models.CASCADE)
     content = models.TextField("후기")
     
--- users/models.py
+++ users/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class CommonModel(models.Model):
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    # db_status??
+    
+    class Meta:
+        abstract = True
+        # class Meta 를 선언함으로써, 다른 모델들이 상속 받을수 있는 모델이 됨
