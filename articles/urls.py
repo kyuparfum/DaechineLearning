e453b0a2028e840e889ae9c9a361c2469a7a2385
Choi Jinkyu <kyuparfum@naver.com>
2023-05-22 19:30:27 +0900

    music api add

--- articles/urls.py
+++ articles/urls.py
@@ -1,6 +1,10 @@
 from django.contrib import admin
 from django.urls import path
+from articles import views
 
 urlpatterns = [
     # path('/', admin.site.urls),
+    path("music/api", views.MusicApiDetail.as_view(), name="music_api"),
+    path("music/api/genre", views.MusicGenreApiDetail.as_view(),
+         name="music_api_genre"),
 ]
