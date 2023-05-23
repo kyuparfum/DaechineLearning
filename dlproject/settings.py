06d214136c915f1f31974d0168a694f0d266ae92
Lee Hyeonsik <hyeon5819@gmail.com>
2023-05-23 11:48:36 +0900

    comment 모델 수정, emoticon관련 serializers, views 추가, 개발단계 urlpatterns추가

--- dlproject/settings.py
+++ dlproject/settings.py
@@ -146,7 +146,7 @@ CORS_ALLOW_ALL_ORIGINS = True
 
 
 SIMPLE_JWT = {
-    "ACCESS_TOKEN_LIFETIME": timedelta(minutes=3),
+    "ACCESS_TOKEN_LIFETIME": timedelta(minutes=720),
     "REFRESH_TOKEN_LIFETIME": timedelta(days=1),
     "ROTATE_REFRESH_TOKENS": False,
     "BLACKLIST_AFTER_ROTATION": False,
