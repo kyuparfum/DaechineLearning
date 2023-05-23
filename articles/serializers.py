cd54ae62a5f15b2183dd3fb4ff35352037eef401
Choi Jinkyu <kyuparfum@naver.com>
2023-05-23 17:50:06 +0900

    album image add

--- articles/serializers.py
+++ articles/serializers.py
@@ -3,7 +3,7 @@ from rest_framework import serializers
 class MusicSerializer(serializers.Serializer):
     name = serializers.SerializerMethodField()
     artist = serializers.SerializerMethodField()
-
+    album = serializers.SerializerMethodField()
     def get_name(self, obj):
         return obj.get('name')
 
@@ -13,3 +13,22 @@ class MusicSerializer(serializers.Serializer):
             return artists[0].get('name')
         else:
             return None
+    def get_album(self, obj):
+        album = obj.get('album', {})
+        return {
+            'name': album.get('name'),
+            'release_date': album.get('release_date'),
+            'images': album.get('images'),
+            'artist_name': album.get('artists', [{}])[0].get('name'),
+        }
+
+class ImageSerializer(serializers.Serializer):
+    url = serializers.URLField()
+    width = serializers.IntegerField()
+    height = serializers.IntegerField()
+
+class ArtistSerializer(serializers.Serializer):
+    id = serializers.CharField()
+    name = serializers.CharField()
+    href = serializers.CharField()
+    images = ImageSerializer(many=True, required=False, read_only=True)
