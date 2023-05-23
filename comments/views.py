3b80c5dffd461e91267a8edadb0ec9ab7211589a
jmpop97 <99373073+jmpop97@users.noreply.github.com>
2023-05-23 11:54:34 +0900

    Update views.py

--- comments/views.py
+++ comments/views.py
@@ -2,7 +2,8 @@ from rest_framework.generics import get_object_or_404
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from rest_framework import status, permissions
-from comments.serializers import CommentSerializer, CommentCreateSerializer, EmoticonSerializer, EmoticonImagesSerializer
+from comments.serializers import CommentSerializer, CommentCreateSerializer,\
+EmoticonSerializer, EmoticonImagesSerializer
 from comments.models import Comment, Emoticon, EmoticonImages, UserBoughtEmoticon
 
 
