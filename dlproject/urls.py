743517386c20937b8740868d23c965b107af5a7c
jmpop <pjm970128@gmail.com>
2023-05-24 20:36:06 +0900

    record

--- dlproject/urls.py
+++ dlproject/urls.py
@@ -18,12 +18,14 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from users.views import SoundAI
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('users/', include('users.urls')),
     path('articles/', include('articles.urls')),
     path('comments/', include('comments.urls')),
+    path('sound/', SoundAI.as_view(),name= 'soundai')
 ]
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
