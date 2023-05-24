eea83ac0e4646780db98dc812abad3d8d2be6eb0
jmpop97 <99373073+jmpop97@users.noreply.github.com>
2023-05-24 20:51:18 +0900

    Update serializers.py

--- users/serializers.py
+++ users/serializers.py
@@ -2,7 +2,6 @@ from rest_framework import serializers
 from users.models import User
 from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
 
-
 class Userserializer(serializers.ModelSerializer):
     class Meta:
         model = User
