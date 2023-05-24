6d170192328aa0812f66862f94280637a510f8a9
jmpop97 <99373073+jmpop97@users.noreply.github.com>
2023-05-24 19:06:27 +0900

    Update urls.py

--- articles/urls.py
+++ articles/urls.py
@@ -2,7 +2,6 @@ from django.urls import path
 from articles import views
 
 urlpatterns = [
-    # path('/', admin.site.urls),
     path("music/api", views.MusicApiDetail.as_view(), name="music_api"),
     path("music/api/token", views.GetMusicAccessToken.as_view(),
          name="music_api_token"),
